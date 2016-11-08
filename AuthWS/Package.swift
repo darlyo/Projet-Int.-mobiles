659dff66e2c453feea9795c7ea812be2f7f7f0fb
darlyo <mainguene.kevin@gmail.com>
2016-11-08 16:24:06 +0100

    Swift With Kitura and redis

--- AuthWS/Package.swift
+++ AuthWS/Package.swift
@@ -3,6 +3,8 @@ import PackageDescription
 let package = Package(
     name: "AuthWS",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 1)
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", majorVersion: 1, minor: 1)
     ])
 
