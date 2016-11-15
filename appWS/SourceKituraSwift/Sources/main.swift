fc8d4a07fc5689eb76df93da91f4928a30b87e5a
francois <thomas.le-corre@techvalley.fr>
2016-11-15 17:00:16 +0100

    Modification Request Get Message, calcul distance geo

--- appWS/SourceKituraSwift/Sources/main.swift
+++ appWS/SourceKituraSwift/Sources/main.swift
@@ -7,6 +7,55 @@ import Foundation
 // Create a new router
 let router = Router()
 
+struct MyVariablesGlobale { //Seulement local 
+    static var nbkeys = [Int]()  //nombre de clé totale pour les messages postés
+}
+
+extension Int {
+    var degreesToRadians: Double { return Double(self) * .pi / 180 }
+    var radiansToDegrees: Double { return Double(self) * 180 / .pi }
+}
+extension FloatingPoint {
+    var degreesToRadians: Self { return self * .pi / 180 }
+    var radiansToDegrees: Self { return self * 180 / .pi }
+}
+
+//faire le calcul pour la longitude/latitude  
+    public func Distance(latitudeA_degre: String, longitudeA_degre: String, latitudeB_degre: String, longitudeB_degre: String) -> Float {
+
+        //Convertir les données en float 
+        //let latitudeA_deg_float = (latitudeA_degre as NSString).floatValue
+        let latitudeA_deg_float = Float(latitudeA_degre)
+        let longitudeA_deg_float = Float(longitudeA_degre)
+        let latitudeB_deg_float = Float(latitudeB_degre)
+        let longitudeB_deg_float = Float(longitudeB_degre)
+
+        //Convertir les données de degrés en radian 
+        let latitudeA = Float(latitudeA_deg_float!).degreesToRadians
+        let longitudeA = Float(longitudeA_deg_float!).degreesToRadians
+        let latitudeB = Float(latitudeB_deg_float!).degreesToRadians
+        let longitudeB = Float(longitudeB_deg_float!).degreesToRadians
+
+        let RayonTerre = 6378000 //Rayon de la terre en mètre
+
+        let distanceResult = RayonTerre * (3.14159265/2 - asin(sin(latitudeA) * sin(latitudeA) + cos(longitudeB - longitudeA) * cos(latitudeB) * cos(latitudeA)))
+
+        return distanceResult
+    }
+
+
+// extension NSDate
+// {
+//     convenience
+//       init(dateString:String) {
+//       let dateStringFormatter = NSDateFormatter()
+//       dateStringFormatter.dateFormat = "yyyy-MM-dd"
+//       dateStringFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+//       let d = dateStringFormatter.dateFromString(dateString)!
+//       self.init(timeInterval:0, sinceDate:d)
+//     }
+//  }
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////// Connection to BDD Redis
 let redis = Redis()
 
@@ -38,7 +87,7 @@ redis.connect(host: "", port: 6379) { (redisError: NSError?) in
 
 // Handle HTTP GET requests to /
 // router.get("/app") {
-//  redis.connect(host: "", port: 6379);
+//  redis.connect(host: "", port: 6379)
 //     request, response, next in
 //     response.send("Hello, World!")
 //     next()
@@ -57,23 +106,101 @@ router.get("/app/message") { request, response, next in //Renvoyer les informati
         return 
     //{"longitude": "132.12", "latitude": "50.56", "radius": "35.24", "date": "12/11/17", "hours": "16.05"}
     }
+    var longitudeMobile = ""
+    var latitudeMobile = ""
+    var radiusMobile = ""
+    var dateMobile = ""
+    var hoursMobile = ""
+
     switch(parsedBody) {
     case .json(let jsonBody):
-            let longitude = jsonBody["longitude"].string ?? ""
-            let latitude = jsonBody["latitude"].string ?? ""
-            let radius = jsonBody["radius"].string ?? ""
-            let date = jsonBody["date"].string ?? ""
-            let hours = jsonBody["hours"].string ?? ""
+            longitudeMobile = jsonBody["longitude"].string ?? ""
+            latitudeMobile = jsonBody["latitude"].string ?? ""
+            radiusMobile = jsonBody["radius"].string ?? ""
+            dateMobile = jsonBody["date"].string ?? ""
+            hoursMobile = jsonBody["hours"].string ?? ""
 
     default:
         break
     }
-    //faire le calcul pour la longitude/latitude et radius 
+    //Recupérer tous les messages via la BDD
+    //Selon le nombre de clés on fait une boucle pour récupérer tous les messages 
 
-    //Recevoir les topics via la BDD 
+    //for nbkeys = 1; nbkeys <= 50; ++nbkeys {
+    for i in stride(from: 0, to: 50, by: 1){
+        redis.hgetall(String(i)) {(responseRedis:[String: RedisString], redisError: NSError?) in
+            if let error = redisError {
+                response.send("Error")
+            }
 
+            let valueLongitude = ""
+            let valueLatitude = ""
+            let valuePopularity = ""
+            let valueDate = ""
+            let valueHours = ""
+            let valueTopic = ""
+            var json = JSON([:])
+            var jsonGlobal = JSON([:])
+            //Recuperer les resultats
+            switch(response) {
+            case .Array(let responses):
+                for idx in stride(from: 0, to: responses.count-1, by: 12) {
+                    switch(responses[idx]) {
+                        case .StringValue(let longitude):
+                        
+                            responses[idx+1].StringValue(valueLongitude)
+                            json["longitude"].StringValue = valueLongitude
+                            
+                        case .StringValue(let latitude):
+                        
+                            responses[idx+1].StringValue(valueLatitude)
+                            json["latitude"].StringValue = valueLatitude
+                               
+                        case .StringValue(let popularity):
 
+                            responses[idx+1].StringValue(valuePopularity)
+                            json["popularity"].StringValue = valuePopularity
+                                
+                        case .StringValue(let date):
 
+                            responses[idx+1].StringValue(valueDate)
+                            json["date"].StringValue = valueDate
+                                
+                        case .StringValue(let hours):
+
+                            responses[idx+1].StringValue(valueHours)
+                            json["hours"].StringValue = valueHours
+                                        
+                        case .StringValue(let topic):
+
+                            responses[idx+1].StringValue(valueTopic)
+                            json["topic"].StringValue = valueTopic
+                                        
+                        case .Error(let err):
+                            error = self.createError("Error: \(err)", code: 1)
+                        default:
+                            error = self.createUnexpectedResponseError(response)
+                    }
+                }
+                let radiusFloat = Float(radiusMobile) //Convertir le radius de string -> float 
+                //Récupérer les éléments de chaque message en json 
+                var resultDistance: Float
+
+                resultDistance = Distance(latitudeA_degre: latitudeMobile, longitudeA_degre: longitudeMobile, latitudeB_degre: json["latitude"], longitudeB_degre:json["longitude"]) //Appel de la fonction distance
+                if resultDistance < radiusFloat! * 1000 {  //Comparaison de la distance entre les deux points et le radius
+
+                    //jsonGlobal.append(json) 
+                    //jsonGlobal["\(i)"].append(json) 
+                }
+
+            default:
+                break    
+            }
+        }
+    }
+    
+
+    //Renvoyer le jsonGlobal au client  
 
 }
     
