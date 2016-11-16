adad3c6cd9c0588df649c10976104b6db6fe1763
darlyo <mainguene.kevin@gmail.com>
2016-11-16 12:26:55 +0100

    import starter bluemix

--- appWS/SourceKituraSwift/Package.swift
+++
@@ -1,9 +0,0 @@
-import PackageDescription
-
-
-let package = Package(
-    name: "projectTest",
-    dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", majorVersion: 1, minor: 1)
-	])
